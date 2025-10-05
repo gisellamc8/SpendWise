8f8f8146bbe6a65bf6078d74d0cb6b007508b03d
Gabrielle Cucu <gcucu@saintpeters.edu>
2025-10-05 08:14:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3148477478-44f07",
+  "appId": "1:371837130707:web:ca74b648729b9fb7b34126",
+  "apiKey": "AIzaSyDjPjwSGiO5PYFEHMk6V5Wq1wO-GBi3IWc",
+  "authDomain": "studio-3148477478-44f07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371837130707"
+};
